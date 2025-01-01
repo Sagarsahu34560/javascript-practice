d18bf5934a5ec341e86ad22f491d2f528cc3159f
Sagarsahu34560 <54035119+Sagarsahu34560@users.noreply.github.com>
2025-01-01 14:37:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat the JS code as newer version
+
+//alert( 3 + 3) //we are using nodejs, not browser
+
+console.js (3
+    + 3)  // code readabiity should be high
+
+console.log("SSS")
+
+// Datatypes
+
+// number => 2 to power 53
+// bigInt 
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null); // object
